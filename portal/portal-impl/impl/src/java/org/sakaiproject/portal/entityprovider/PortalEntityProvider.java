0826176a8a14ec56a7c931fe1cfe09deaee24f27
Mitch Golden <mgolden@noodle.com>
2016-06-14 15:10:40 +0000

    SAK-30461 (bullhorns)

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/entityprovider/PortalEntityProvider.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/entityprovider/PortalEntityProvider.java
@@ -6,6 +6,8 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -32,6 +34,7 @@ import org.sakaiproject.entitybroker.exception.EntityException;
 
 import org.sakaiproject.profile2.logic.ProfileConnectionsLogic;
 import org.sakaiproject.profile2.logic.ProfileLogic;
+import org.sakaiproject.profile2.model.Person;
 import org.sakaiproject.profile2.model.UserProfile;
 import org.sakaiproject.profile2.util.ProfileConstants;
 
@@ -39,6 +42,8 @@ import org.sakaiproject.tool.api.Session;
 import org.sakaiproject.tool.api.SessionManager;
 import org.sakaiproject.util.ResourceLoader;
 
+import org.sakaiproject.portal.api.PortalService;
+import org.sakaiproject.portal.beans.BullhornAlert;
 import org.sakaiproject.portal.beans.PortalNotifications;
 
 import lombok.Setter;
@@ -48,11 +53,14 @@ import lombok.extern.slf4j.Slf4j;
  * An entity provider to serve Portal information
  * 
  */
-@Slf4j
+@Setter @Slf4j
 public class PortalEntityProvider extends AbstractEntityProvider implements AutoRegisterEntityProvider, Outputable, ActionsExecutable, Describeable {
 
 	public final static String PREFIX = "portal";
 	public final static String TOOL_ID = "sakai.portal";
+
+	@Setter
+	private PortalService portalService;
 
 	@Setter
 	private ProfileConnectionsLogic profileConnectionsLogic;
@@ -162,5 +170,140 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 		} catch (IOException ioe) {
 			throw new EntityException("Failed to format profile.", ref.getReference());
 		}
+    }
+
+	@EntityCustomAction(action = "socialAlerts", viewKey = EntityView.VIEW_LIST)
+	public ActionReturn getSocialAlerts(EntityView view) {
+
+		String currentUserId = getCheckedCurrentUser();
+
+		List<BullhornAlert> alerts = portalService.getSocialAlerts(currentUserId);
+
+		ResourceLoader rl = new ResourceLoader("bullhorns");
+
+		if (alerts.size() > 0) {
+			Map<String, Object> data = new HashMap();
+			data.put("alerts", alerts);
+			data.put("i18n", rl);
+
+			return new ActionReturn(data);
+		} else {
+			Map<String, String> i18n = new HashMap();
+			i18n.put("noAlerts", rl.getString("noAlerts"));
+
+			Map<String, Object> data = new HashMap();
+			data.put("message", "NO_ALERTS");
+			data.put("i18n", i18n);
+
+			return new ActionReturn(data);
+		}
+	}
+
+	@EntityCustomAction(action = "clearSocialAlert", viewKey = EntityView.VIEW_LIST)
+	public boolean clearSocialAlert(Map<String, Object> params) {
+
+		String currentUserId = getCheckedCurrentUser();
+
+		try {
+			long alertId = Long.parseLong((String) params.get("id"));
+			return portalService.clearSocialAlert(currentUserId, alertId);
+		} catch (Exception e) {
+			log.error("Failed to clear social alert", e);
+		}
+
+		return false;
+	}
+
+	@EntityCustomAction(action = "clearAllSocialAlerts", viewKey = EntityView.VIEW_LIST)
+	public boolean clearAllSocialAlerts(Map<String, Object> params) {
+
+		String currentUserId = getCheckedCurrentUser();
+
+		try {
+			return portalService.clearAllSocialAlerts(currentUserId);
+		} catch (Exception e) {
+			log.error("Failed to clear all social alerts", e);
+		}
+
+		return false;
+	}
+
+	@EntityCustomAction(action = "academicAlerts", viewKey = EntityView.VIEW_LIST)
+	public ActionReturn getAcademicAlerts(EntityView view) {
+
+		String currentUserId = getCheckedCurrentUser();
+
+		List<BullhornAlert> alerts = portalService.getAcademicAlerts(currentUserId);
+
+		ResourceLoader rl = new ResourceLoader("bullhorns");
+
+		if (alerts.size() > 0) {
+			Map<String, Object> data = new HashMap();
+			data.put("alerts", alerts);
+			data.put("i18n", rl);
+
+			return new ActionReturn(data);
+		} else {
+			Map<String, String> i18n = new HashMap();
+			i18n.put("noAlerts", rl.getString("noAlerts"));
+
+			Map<String, Object> data = new HashMap();
+			data.put("message", "NO_ALERTS");
+			data.put("i18n", i18n);
+
+			return new ActionReturn(data);
+		}
+	}
+
+	@EntityCustomAction(action = "clearAcademicAlert", viewKey = EntityView.VIEW_LIST)
+	public boolean clearAcademicAlert(Map<String, Object> params) {
+
+		String currentUserId = getCheckedCurrentUser();
+
+		try {
+			long alertId = Long.parseLong((String) params.get("id"));
+			return portalService.clearAcademicAlert(currentUserId, alertId);
+		} catch (Exception e) {
+			log.error("Failed to clear academic alert", e);
+		}
+
+		return false;
+	}
+
+	@EntityCustomAction(action = "clearAllAcademicAlerts", viewKey = EntityView.VIEW_LIST)
+	public boolean clearAllAcademicAlerts(Map<String, Object> params) {
+
+		String currentUserId = getCheckedCurrentUser();
+
+		try {
+			return portalService.clearAllAcademicAlerts(currentUserId);
+		} catch (Exception e) {
+			log.error("Failed to clear all academic alerts", e);
+		}
+
+		return false;
+	}
+
+	@EntityCustomAction(action = "bullhornCounts", viewKey = EntityView.VIEW_LIST)
+	public ActionReturn getBullhornCounts(EntityView view) {
+
+		String currentUserId = getCheckedCurrentUser();
+
+		Map<String, Integer> counts = new HashMap();
+		counts.put("academic", portalService.getAcademicAlertCount(currentUserId));
+		counts.put("social", portalService.getSocialAlertCount(currentUserId));
+
+		return new ActionReturn(counts);
+	}
+
+	private String getCheckedCurrentUser() throws SecurityException {
+
+		String currentUserId = developerHelperService.getCurrentUserId();
+
+		if (StringUtils.isBlank(currentUserId)) {
+			throw new SecurityException("You must be logged in to use this service");
+		} else {
+            return currentUserId;
+        }
 	}
 }
