6ff07014b5c5827d5a709dc0ae007c2c5cd10154
Adrian Fish <adrian.r.fish@gmail.com>
2016-06-14 11:48:33 +0100

    SAK-31355 Added Morpheus formatted profile popup (#2817)

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/entityprovider/PortalEntityProvider.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/entityprovider/PortalEntityProvider.java
@@ -1,12 +1,21 @@
 package org.sakaiproject.portal.entityprovider;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Locale;
 import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+import org.apache.velocity.runtime.RuntimeConstants;
 
+import org.sakaiproject.component.api.ServerConfigurationService;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.AutoRegisterEntityProvider;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.Describeable;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.Outputable;
@@ -20,14 +29,18 @@ import org.sakaiproject.entitybroker.entityprovider.extension.ActionReturn;
 import org.sakaiproject.entitybroker.entityprovider.extension.Formats;
 import org.sakaiproject.entitybroker.exception.EntityException;
 
+import org.sakaiproject.profile2.logic.ProfileConnectionsLogic;
+import org.sakaiproject.profile2.logic.ProfileLogic;
+import org.sakaiproject.profile2.model.UserProfile;
+import org.sakaiproject.profile2.util.ProfileConstants;
+
 import org.sakaiproject.tool.api.Session;
 import org.sakaiproject.tool.api.SessionManager;
+import org.sakaiproject.util.ResourceLoader;
 
 import org.sakaiproject.portal.api.PortalService;
 import org.sakaiproject.portal.beans.BullhornAlert;
 import org.sakaiproject.portal.beans.PortalNotifications;
-
-import org.sakaiproject.util.ResourceLoader;
 
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -48,6 +61,30 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 	@Setter
 	private SessionManager sessionManager;
 
+	@Setter
+	private ProfileConnectionsLogic profileConnectionsLogic;
+
+	@Setter
+	private ProfileLogic profileLogic;
+
+	@Setter
+	private ServerConfigurationService serverConfigurationService;
+
+	private Template formattedProfileTemplate = null;
+
+	public void init() {
+
+		VelocityEngine ve = new VelocityEngine();
+		ve.setProperty("resource.loader", "class");
+		ve.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+		try {
+			ve.init();
+			formattedProfileTemplate = ve.getTemplate("org/sakaiproject/portal/entityprovider/profile-popup.vm");
+		} catch (Exception e) {
+			log.error("Failed to load formatted_profile.vm", e);
+		}
+	}
+
 	public String getEntityPrefix() {
 		return PREFIX;
 	}
@@ -56,9 +93,9 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 		return TOOL_ID;
 	}
 
-        public String[] getHandledOutputFormats() {
-            return new String[] { Formats.TXT ,Formats.JSON, Formats.HTML};
-        }
+	public String[] getHandledOutputFormats() {
+		return new String[] { Formats.TXT ,Formats.JSON, Formats.HTML};
+	}
 
 	// So far all we do is errors to solve SAK-29531
 	// But this could be extended...
@@ -212,5 +249,57 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 		} else {
             return currentUserId;
         }
+    }
+
+	@EntityCustomAction(action="formatted",viewKey=EntityView.VIEW_SHOW)
+	public ActionReturn getFormattedProfile(EntityReference ref) {
+
+		String currentUserId = developerHelperService.getCurrentUserId();
+
+		ResourceLoader rl = new ResourceLoader(currentUserId, "profile-popup");
+
+		UserProfile userProfile = (UserProfile) profileLogic.getUserProfile(ref.getId());
+
+		String connectionUserId = userProfile.getUserUuid();
+
+		VelocityContext context = new VelocityContext();
+		context.put("displayName", userProfile.getDisplayName());
+		context.put("email", userProfile.getEmail());
+		context.put("currentUserId", currentUserId);
+		context.put("connectionUserId", connectionUserId);
+		context.put("requestMadeLabel", rl.getString("connection.requested"));
+		context.put("cancelLabel", rl.getString("connection.cancel"));
+		context.put("incomingRequestLabel", rl.getString("connection.incoming.request"));
+		context.put("removeConnectionLabel", rl.getString("connection.remove"));
+		context.put("acceptLabel", rl.getString("connection.accept"));
+		context.put("rejectLabel", rl.getString("connection.reject"));
+		context.put("addConnectionLabel", rl.getString("connection.add"));
+
+		boolean connectionsEnabled = serverConfigurationService.getBoolean("profile2.connections.enabled",
+					ProfileConstants.SAKAI_PROP_PROFILE2_CONNECTIONS_ENABLED);
+
+		if (connectionsEnabled && !currentUserId.equals(connectionUserId)) {
+
+			int connectionStatus = profileConnectionsLogic.getConnectionStatus(currentUserId, connectionUserId);
+
+			if (connectionStatus == ProfileConstants.CONNECTION_CONFIRMED) {
+				context.put("connected" , true);
+			} else if (connectionStatus == ProfileConstants.CONNECTION_REQUESTED) {
+				context.put("requested" , true);
+			} else if (connectionStatus == ProfileConstants.CONNECTION_INCOMING) {
+				context.put("incoming" , true);
+			} else {
+				context.put("unconnected" , true);
+			}
+		}
+
+		StringWriter writer = new StringWriter();
+
+		try {
+			formattedProfileTemplate.merge(context, writer);
+			return new ActionReturn(Formats.UTF_8, Formats.HTML_MIME_TYPE, writer.toString());
+		} catch (IOException ioe) {
+			throw new EntityException("Failed to format profile.", ref.getReference());
+		}
 	}
 }
